43108809b755c3e2aec17a4100940cb284f425a5
gonzaloiv <aelthalion@gmail.com.com>
2026-05-25 09:43:04 +0200

    Fix skip button behaviour

--- Assets/_Game/Features/TTS/Scripts/TTSHelper.cs
+++ Assets/_Game/Features/TTS/Scripts/TTSHelper.cs
@@ -4,8 +4,6 @@ using System;
 using DigitalLove.Game.Levels;
 using DigitalLove.Global;
 using DigitalLove.DataAccess;
-using System.Collections.Generic;
-using System.Linq;
 using Reflex.Attributes;
 using DigitalLove.UI.Behaviours;
 
@@ -14,7 +12,6 @@ namespace DigitalLove.Game.TTS
     public class TTSHelper : MonoBehaviour
     {
         [SerializeField] private MarkdownTTSVoiceWrapper ttsVoiceWrapper;
-        [SerializeField] private StringValue[] tips;
         [SerializeField] private SubtitlesLikeFollow subtitlesLikeFollow;
         [SerializeField] private Transform defaultPoint;
 
@@ -27,31 +24,9 @@ namespace DigitalLove.Game.TTS
             SaveCookie(key);
         }
 
-        public bool HasIntro(RoundData roundData)
-        {
-            Entry[] entries = ttsVoiceWrapper.GetEntriesByKey(roundData.IntroKey);
-            return entries.HasAnyValid();
-        }
-
         public void SayRoundIntro(RoundData roundData, Action onComplete)
         {
             Say(roundData.IntroKey, onComplete);
-        }
-
-        public void SayRandomTipIfAvailable(Action onComplete)
-        {
-            PlayerData playerData = memoryDataClient.Get<PlayerData>();
-            List<StringValue> availableTips = tips.Where(m => !playerData.HasCookie(m.value)).ToList();
-            if (availableTips.Count == 0)
-            {
-                onComplete();
-                return;
-            }
-            else
-            {
-                StringValue tip = availableTips[UnityEngine.Random.Range(0, availableTips.Count)];
-                Say(tip.value, onComplete);
-            }
         }
 
         private async void SaveCookie(string key)
@@ -60,11 +35,6 @@ namespace DigitalLove.Game.TTS
             Cookie cookie = playerData.GetOrCreateCookie(key);
             playerData.AddCookie(cookie);
             await unityPlayerDataClient.Put(playerData);
-        }
-
-        public void SayAfter(float secs, string key, Action onComplete)
-        {
-            this.InvokeDelayed(secs, () => Say(key, onComplete));
         }
 
         public void SetInFrontOfCameraOrDefault(bool isInFrontOfCamera)
